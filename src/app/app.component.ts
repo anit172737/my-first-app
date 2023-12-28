3a2c53ae741b9e3c5f095633ec4c1c6e64061465
anit172737 <anitdhadve17@gmail.com>
2023-12-28 10:42:40 +0530

    tdf form created with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'my-first-app';
+  topics = [
+    'angular','css', 'js', 'html'
+  ]
+
+  userModel = new User('', 'jon@gmail.com', 234324234, 'angular', 'morning', true)
+  
+  onSubmit() {
+    console.log('userModel', this.userModel)
+  }
 }
